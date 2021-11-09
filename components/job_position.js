945cb7a3356a87426692b7d8545295a0b15af949
LiamMahoney <liammahoney96@gmail.com>
2021-11-08 19:38:06 -0600

    updated indentation + spacing + description color

--- components/job_position.js
+++ components/job_position.js
@@ -1,17 +1,20 @@
 import {
     Flex,
     Heading,
-    Text
+    Text,
+    useColorModeValue
 } from "@chakra-ui/react";
 
 export default function JobPosition({title, description}) {
+
+    const descColor = useColorModeValue('gray.600', 'gray.400');
 
     return (
         <Flex flexDirection="column" marginTop={3} paddingLeft={{md: 4}}>
             <Heading size="sm">
                 {title}
             </Heading>
-            <Text marginLeft={4}>
+            <Text marginLeft={4} color={descColor} marginTop={2}>
                 {description}
             </Text>
         </Flex>
