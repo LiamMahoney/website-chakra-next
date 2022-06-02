f9aa5b6801b532ff3185a7f4fec7020a8d09b838
LiamMahoney <liammahoney96@gmail.com>
2022-06-01 22:00:30 -0500

    added terminal favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from 'next/head';
 import theme from "../components/theme/theme";
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <ChakraProvider theme={theme}>
+            <Head>
+                <link rel="shortcut icon" href="/favicon.svg" />
+            </Head>
             <Component {...pageProps} />
         </ChakraProvider>
     )
