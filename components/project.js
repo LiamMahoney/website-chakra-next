35eadbee19f0f96f7fb50fa184b240e67287c89e
LiamMahoney <liammahoney96@gmail.com>
2022-07-06 21:17:38 -0500

    updated layout

--- components/project.js
+++ components/project.js
@@ -13,33 +13,35 @@ export default function Project({ project }) {
             flexDirection="column"
             margin={8}
         > 
-            <Flex
+            <Flex 
                 justifyContent="space-between"
+                flexDirection={{'base': 'column', 'md': 'row'}}
+                alignItems={{'base': 'flex-start', 'md': 'flex-end'}}
             >
-                <Link 
-                    href="https://beersheets.liammahoney.io"
-                    isExternal={true}
-                    _hover={{
-                        textDecoration: "underline",
-                        textDecorationThickness: "2px"
-                    }}
-                    _active="unset"
-                    _focus="unset"
-                >
-                    <Heading size="md">
-                        {project.attributes.Title}
-                    </Heading>
-                </Link>
                 <Flex
-                    alignItems="center"
-                    justifyContent="flex-end"
-                    flexGrow={1}
+                    justifyContent="space-between"
+                    width="100%"
                 >
+                    <Link 
+                        href={project.attributes.url}
+                        isExternal={true}
+                        _hover={{
+                            textDecoration: "underline",
+                            textDecorationThickness: "2px"
+                        }}
+                        _active="unset"
+                        _focus="unset"
+                    >
+                        <Heading size="md">
+                            {project.attributes.Title}
+                        </Heading>
+                    </Link>
                     <Link
                         href={project.attributes.repository_url}
                         isExternal={true}
                         _active="unset"
                         _focus="unset"
+                        height={6}
                     >
                         <Icon 
                             as={BsCode}
@@ -50,27 +52,35 @@ export default function Project({ project }) {
                 </Flex>
             </Flex>
             <Flex
-                marginTop={0}
-                justifyContent="flex-start"
+                flexWrap={{'base': 'wrap-reverse', 'md': 'nowrap'}}
+                justifyContent={{'md': 'space-between'}}
             >
-                {project.attributes.project_tags.data.map((tag) => {
-                    return (
-                        <Tag 
-                            key={tag.id}
-                            marginRight={4}
-                            size="sm"
-                            sx={{backgroundColor: tagColor}}
-                        >
-                            {tag.attributes.technology}
-                        </Tag>
-                    )
-                })}
+                <Text
+                    marginTop={{'base': 1, 'md': 2}}
+                    width={{'base': '100%', 'md': 'auto'}}
+                >
+                    {project.attributes.description}
+                </Text>
+                <Flex
+                    marginTop={{'base': 1, 'md': 0}}
+                    flexDir={{'base': 'row', 'md': 'column'}}
+                    flexShrink={{'md': 0}}
+                    alignItems={{'base': 'flex-start', 'md': 'flex-end'}}
+                >
+                    {project.attributes.project_tags.data.map((tag) => {
+                        return (
+                            <Tag 
+                                key={tag.id}
+                                marginTop={{'base': 0, 'md': 1}}
+                                marginRight={{'base': 1, 'md': 0}}                                size="sm"
+                                sx={{backgroundColor: tagColor}}
+                            >
+                                {tag.attributes.technology}
+                            </Tag>
+                        )
+                    })}
+                </Flex>
             </Flex>
-            <Text
-                marginTop={1}
-            >
-                {project.attributes.description}
-            </Text>
         </Flex>
     )
 }
